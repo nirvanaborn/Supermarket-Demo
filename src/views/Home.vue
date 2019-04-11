62e2b8b68386317bac8ddf4ca8051be4f6407f07
郭继隆 <1846055905@qq.com>
2019-04-11 18:37:28 +0800

    整理了项目的结构，删除了一些冗余的文件和代码

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,33 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="quit">注销</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import { setCookie, getCookie, delCookie } from "../assets/js/cookie";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     HelloWorld
+  },
+  mounted() {
+    if (!getCookie("loginName")) {
+      this.$router.push({ path: "/login" });
+    }
+  },
+  methods: {
+    quit() {
+      delCookie("loginName");
+      console.log("cookie删除成功");
+      setTimeout(function() {
+        location.reload();
+      }, 3000);
+    }
   }
-}
+};
 </script>
