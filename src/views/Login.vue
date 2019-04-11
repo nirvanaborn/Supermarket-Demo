fdb394791de4cc64bf6fffb07d15449a74e28f2d
郭继隆 <1846055905@qq.com>
2019-04-11 18:40:19 +0800

    新增了登录注册页

---
+++ src/views/Login.vue
@@ -0,0 +1,168 @@
+<template>
+  <div class="login">
+    <div class="header">
+      <img src="../assets/logo.png" alt="logo" id="logo">
+    </div>
+    <div class="container">
+      <div class="c-r">
+        <h1>用户登录</h1>
+        <div class="item">
+          <label for="loginName">用户名：</label>
+          <input type="text" id="loginName" v-model="loginName">
+        </div>
+        <div class="item">
+          <label for="password">密&nbsp;&nbsp;&nbsp;码：</label>
+          <input type="password" id="password" v-model="password">
+        </div>
+        <div class="item">
+          <a href="javascript:void(0);">忘记密码</a>
+          <a href="javascript:void(0);">立即注册</a>
+        </div>
+        <div class="item">
+          <button id="btn-login" @click="login">登&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;录</button>
+        </div>
+      </div>
+    </div>
+    <div class="footer">
+      By：North University of China 郭继隆
+      <br>email：1846055905@qq.com
+      <br>phone：18306897726
+    </div>
+  </div>
+</template>
+<script>
+import { getCookie, setCookie } from "../assets/js/cookie";
+export default {
+  name: "login",
+  data() {
+    return {
+      loginName: "",
+      password: ""
+    };
+  },
+  mounted() {
+    console.log(getCookie("loginName"));
+  },
+  methods: {
+    login() {
+      var vm = this;
+      if (!this.loginName || !this.password) {
+        alert("请输入用户名或密码");
+        return;
+      }
+      let data = {
+        loginName: this.loginName,
+        password: this.password
+      };
+      this.axios.post("/api/login", data).then(function(res) {
+        if (res.data.code != 200) {
+          alert(res.data.message);
+          return;
+        }
+        setCookie("loginName", vm.loginName, 1000 * 60);
+        vm.$router.push({ path: "/" });
+      });
+    }
+  }
+};
+</script>
+<style scoped>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  font-size: 14px;
+  box-sizing: border-box;
+  height: 100%;
+}
+
+a {
+  text-decoration-line: none;
+  color: #000;
+}
+
+#logo {
+  margin-left: 50px;
+}
+
+.container {
+  background-color: #99cc99;
+  height: calc(100% - 180px);
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+}
+
+.c-r {
+  width: 300px;
+  height: 300px;
+  border: 2px solid transparent;
+  border-radius: 10px;
+  padding: 10px;
+  margin-right: 100px;
+  background-color: #fff;
+  align-items: center;
+  color: #99cc99;
+  font-weight: 700;
+  -webkit-user-select: none;
+}
+
+.c-r h1 {
+  padding-left: 10px;
+  color: #99cc99;
+}
+
+.c-r .item {
+  margin: 20px 0;
+  padding-left: 10px;
+  font-size: 16px;
+}
+
+.c-r .item input {
+  width: 180px;
+  height: 30px;
+  padding-left: 10px;
+  line-height: 30px;
+  font-size: 16px;
+  outline: none;
+  border: 1px solid #99cc99;
+}
+
+.c-r .item:nth-of-type(3) {
+  display: flex;
+  justify-content: flex-end;
+  margin-right: 20px;
+}
+
+.c-r .item:nth-of-type(3) a {
+  display: block;
+  margin: 0 5px;
+  color: #99cc99;
+}
+
+.c-r .item:nth-of-type(4) {
+  display: flex;
+  justify-content: center;
+}
+
+.c-r button {
+  color: #fff;
+  width: 200px;
+  height: 30px;
+  text-align: center;
+  background-color: #99cc99;
+  border: 1px solid #ccc;
+  border-radius: 14px;
+  outline: none;
+  margin: 0 auto;
+  font-weight: 700;
+}
+
+.footer {
+  padding: 10px 0;
+  height: 60px;
+  text-align: center;
+  color: #99cc99;
+  font-weight: 700;
+}
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <div class="header">
+      <img src="../assets/logo.png" alt="logo" id="logo">
+    </div>
+    <div class="container">
+      <div class="c-r">
+        <h1>用户注册</h1>
+        <form action>
+          <div class="item">
+            <label>用户类型：</label>
+            <input type="radio" name="user-type" id="admin">
+            <label for="admin">超市管理员</label>
+            <input type="radio" name="user-type" id="personal-user">
+            <label for="personal-user">个人用户</label>
+          </div>
+          <div class="item">
+            <label for="loginName">用户名：</label>
+            <input type="text" id="loginName">
+          </div>
+          <div class="item">
+            <label for="password">密&nbsp;&nbsp;&nbsp;码：</label>
+            <input type="password" id="password">
+          </div>
+          <div class="item">
+            <label for="password">确认密码：</label>
+            <input type="password" id="password">
+          </div>
+          <div class="item">
+            <label for="email">邮箱：</label>
+            <input type="email" id="email">
+          </div>
+          <div class="item">
+            <label for="phone">手机：</label>
+            <input type="phone" id="phone">
+          </div>
+          <div class="item">
+            <label for="yzm">验证码：</label>
+            <input type="text" id="input" value>
+            <input type="button" id="code" onclick="createCode()">
+            <input type="button" id="btn-check" value="验" onclick="validate()">
+          </div>
+
+          <div class="item">
+            <button id="btn-registe">注&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;册</button>
+          </div>
+        </form>
+      </div>
+      <img src="../../public/images/flower.png" alt="left background-image" class="l-bgimg">
+    </div>
+    <div class="footer">
+      By：North University of China 郭继隆
+      <br>email：1846055905@qq.com
+      <br>phone：18306897726
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    name:'register'
+};
+</script>
+<style scoped>
+</style>
