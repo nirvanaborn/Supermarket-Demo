2ec0f5d229a892097726557056e487414ecc66cf
郭继隆 <1846055905@qq.com>
2019-04-15 20:51:15 +0800

    将写好的RotationImg组件使用在home中

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <div class="header">
+      <router-link :to="loginOrpersonalSpaceUrl">{{username}}</router-link>
+    </div>
+    <div class="main">
+      <div class="m-t">
+        <div class="side">我是侧边导航栏</div>
+        <div class="rotationImg">我是轮播图</div>
+        <RotationImg></RotationImg>
+        <div class="discount">我是右侧限时折扣</div>
+      </div>
+      <div class="item">A</div>
+      <div class="item">B</div>
+      <div class="item">C</div>
+      <div class="item">D</div>
+    </div>
+    <div class="footer">我是底部</div>
     <button @click="quit">注销</button>
   </div>
 </template>
@@ -8,13 +24,27 @@
 <script>
 // @ is an alias to /src
 import { setCookie, getCookie, delCookie } from "../assets/js/cookie";
+import RotationImg from "@/components/RotationImg.vue";
 
 export default {
   name: "home",
-  components: {},
+  data() {
+    return {
+      username: "您好，请登录",
+      loginOrpersonalSpaceUrl: ""
+    };
+  },
+  components: {
+    RotationImg
+  },
   mounted() {
     if (!getCookie("loginName")) {
-      this.$router.push({ path: "/login" });
+      console.log("请先登录");
+      this.loginOrpersonalSpaceUrl = "/login";
+      // this.$router.push({ path: "/login" });
+    } else {
+      this.username = getCookie("loginName");
+      this.loginOrpersonalSpaceUrl = "/personspace";
     }
   },
   methods: {
