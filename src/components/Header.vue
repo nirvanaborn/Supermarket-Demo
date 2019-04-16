11fc67ab3c24a079e0b442f95a0f02c512a25870
郭继隆 <1846055905@qq.com>
2019-04-16 17:02:12 +0800

    新建头部组件

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="header">
+    <router-link to="/">
+      <img src="@/assets/logo.png" alt="logo">
+    </router-link>
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: "Header"
+};
+</script>
+<style scoped>
+a {
+  text-decoration: none;
+}
+a:visited {
+  color: #99cc99;
+}
+a:hover {
+  color: #f00;
+}
+.header {
+  width: 1536px;
+  height: 95px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
