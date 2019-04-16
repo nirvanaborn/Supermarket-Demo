ed7ccdbe69cec41fc9103f34b4b17943fd240d3e
郭继隆 <1846055905@qq.com>
2019-04-16 17:01:54 +0800

    新建底部组件

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="footer">
+    By：North University of China 郭继隆
+    <br>email：1846055905@qq.com
+    <br>phone：18306897726
+  </div>
+</template>
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+<style scoped>
+.footer {
+  margin: 0 auto;
+  width: 1536px;
+  height: 80px;
+  padding: 10px 0;
+  height: 60px;
+  text-align: center;
+  color: #99cc99;
+  font-weight: 700;
+}
+</style>
